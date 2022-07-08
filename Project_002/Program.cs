e7f37d7e9480bfab37288e14c0343fea732da9e0
Narimanov Rovshan notebook <rovshan-az@mail.ru>
2022-07-09 00:17:08 +0300

    Задача 2 (Сравнение трёх чисел)

---
+++ Project_002/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int A = new Random().Next(1,101);
+int B = new Random().Next(1,101);
+int C = new Random().Next(1,101);
+int Max = A;
+string Result = " первое число"; 
+Console.WriteLine("Сравнение трёх чисел!");
+Console.Write("Первое число: ");
+Console.WriteLine(A);
+Console.Write("Второе число: ");
+Console.WriteLine(B);
+Console.Write("Третье число: ");
+Console.WriteLine(C);
+
+if (Max < B)
+{
+    Max = B;
+    Result = " второе число";
+}
+
+if (Max < C)
+{
+    Max = C;
+    Result = " третье число";
+}
+
+Console.Write("Из трёх чисел");
+Console.Write(Result);
+Console.Write(" самое большое, равное ");
+Console.WriteLine(Max);
+
