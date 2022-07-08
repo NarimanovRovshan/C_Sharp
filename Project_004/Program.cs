9878f49b4f0588d1c1f85de15045fdb8f0d9f1b8
Narimanov Rovshan notebook <rovshan-az@mail.ru>
2022-07-09 00:54:14 +0300

    Задача 4(Вывод чётных чисел массива)

---
+++ Project_004/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int A = new Random().Next(1,101);
+int Index = 0;
+
+Console.WriteLine("");
+Console.WriteLine("Вывод четных чисел из массива!");
+Console.WriteLine("");
+Console.Write("Дано: массив из ");
+Console.Write(A);
+Console.Write(" чисел, от 0 до ");
+Console.WriteLine(A);
+
+Console.WriteLine("");
+Console.Write("Вывод чётных чисел массива: [");
+
+while(Index <= A)
+{
+    if(Index % 2 == 0)
+    {
+        Console.Write(Index);
+            if(Index != A && Index != A -1)
+            {
+                Console.Write("; ");
+            }
+    }
+    Index++;
+}
+
+Console.WriteLine("]");
+Console.WriteLine("");        
