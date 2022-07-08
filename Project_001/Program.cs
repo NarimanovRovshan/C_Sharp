c030d32786171bd7fe95d578b81e4ea59d37730d
Narimanov Rovshan notebook <rovshan-az@mail.ru>
2022-07-08 23:54:57 +0300

    Задача 1 (Сравнение двух чисел)

--- Project_001/Program.cs
+++ Project_001/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int A = new Random().Next(1,101);
+int B = new Random().Next(1,101);
+
+Console.WriteLine("Сравнение двух чисел!");
+Console.Write("Первое число: ");
+Console.WriteLine(A);
+Console.Write("Второе число: ");
+Console.WriteLine(B);
+
+if (A > B)
+{
+    Console.Write("Число ");
+    Console.Write(A);
+    Console.Write(" больше числа ");
+    Console.Write(B);
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(B);
+    Console.Write(" больше числа ");
+    Console.Write(A);
+}
