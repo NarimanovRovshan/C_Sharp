bdffc58286d80de2cb6004195affa556bcf5f38f
Narimanov Rovshan notebook <rovshan-az@mail.ru>
2022-07-09 00:30:16 +0300

    Задача 3 (Определение чётности числа)

---
+++ Project_003/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int A = new Random().Next(1,101);
+
+Console.WriteLine("Определение чётности числа!");
+Console.Write("Дано число ");
+Console.WriteLine(A);
+
+if(A % 2 == 0)
+{
+    Console.WriteLine("Число чётное!");
+}
+else
+{
+    Console.WriteLine("Число не чётное!");
+}
