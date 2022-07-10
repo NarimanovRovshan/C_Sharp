867aa07da5b7f2640d4d2a90264cc406cf897804
Narimanov Rovshan notebook <rovshan-az@mail.ru>
2022-07-10 23:18:47 +0300

    Задача 6 (Показать третью цифру произвольного числа)

---
+++ Project_006/Program.cs
@@ -0,0 +1,26 @@
+﻿int A = new Random().Next(0,100000);
+Console.WriteLine("");
+Console.WriteLine("Показать третью цифру произвольного числа.");
+Console.WriteLine("");
+
+Console.Write("Дано число ");
+Console.WriteLine(A);
+Console.WriteLine("");
+
+while(true)
+{
+    if(A < 100)
+    {
+        Console.WriteLine("--> Третьей цифры НЕТ!!! <--");
+        Console.WriteLine("");
+        break;
+    }
+    if(A < 1000){
+        A = A % 10;
+        Console.Write("Третья цифра этого числа ");
+        Console.WriteLine(A);
+        Console.WriteLine("");
+        break;
+    }
+    A = A / 10;
+}
