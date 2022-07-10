36e00c277d89e1566b38bdd042e728636e3b45bc
Narimanov Rovshan notebook <rovshan-az@mail.ru>
2022-07-10 22:47:10 +0300

    Задача 5 (Показать вторую цифру трёхзначного числа)

---
+++ Project_005/Program.cs
@@ -0,0 +1,15 @@
+﻿int A = new Random().Next(100,1000);
+Console.WriteLine("");
+Console.WriteLine("Показать вторую цифру трёхзначного числа.");
+Console.WriteLine("");
+
+Console.Write("Дано число ");
+Console.WriteLine(A);
+Console.WriteLine("");
+
+A = A % 100;
+A = A / 10;
+
+Console.Write("Вторая цифра этого числа ");
+Console.WriteLine(A);
+Console.WriteLine("");
