25ba111e0999f2b8a9ececfb0416abd90103e0b9
Narimanov Rovshan notebook <rovshan-az@mail.ru>
2022-07-14 19:59:37 +0300

    Задача 7(Являеться ли число выходным)

---
+++ Project_007/Program.cs
@@ -0,0 +1,16 @@
+﻿int Dey = new Random().Next(1,9);
+Console.WriteLine("");
+Console.Write("Являться ли ");
+Console.Write(Dey);
+Console.WriteLine(" день недели выходным?");
+Console.WriteLine(" ");
+
+if(Dey == 6 || Dey == 7)
+{
+    Console.WriteLine("Да, данный день являться выходным");
+}
+else
+{
+    Console.WriteLine("Нет, данный день не являться выходным");
+}
+Console.WriteLine(" ");
