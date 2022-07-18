a2ee694f5d3551119255b0958c164608510d41ea
Narimanov Rovshan notebook <rovshan-az@mail.ru>
2022-07-18 17:09:34 +0300

    Ввывод массива из 8 чисел

---
+++ Project_013/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ввывод массива из 8 чисел!");
+int[] Numbers = new int[8];
+    Console.Write("[");
+for(int i = 0; i < Numbers.Length; i++)
+{   
+    Numbers[i] = new Random().Next(0,100);
+    if(i == Numbers.Length -1 )
+    {
+        Console.Write($"{Numbers[i]}]");
+        break;
+    }
+    Console.Write($"{Numbers[i]}, ");
+}
