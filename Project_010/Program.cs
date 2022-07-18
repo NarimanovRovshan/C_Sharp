d0df31d6136b15fc82930ee953ada0dfccfaec7a
Narimanov Rovshan notebook <rovshan-az@mail.ru>
2022-07-18 15:51:14 +0300

    Таблица кубов чисел от 1 до N

---
+++ Project_010/Program.cs
@@ -0,0 +1,13 @@
+﻿int Number;
+Console.Write("\nВведите максимальное число до которого будет ввестись расчет: ");
+Number = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= Number; i++)
+{   
+    if(i == Number)
+    {
+        Console.WriteLine($"{Math.Pow(i,3)}.\n");
+        break;
+    }
+    Console.Write($"{Math.Pow(i,3)}, ");
+}
