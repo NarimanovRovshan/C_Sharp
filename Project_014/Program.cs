71a70ac05b2c64cfcfc4996ad010df7e893a10a4
Narimanov Rovshan notebook <rovshan-az@mail.ru>
2022-07-18 18:23:46 +0300

    Вывод количества четных трехзначных чисел из массива

---
+++ Project_014/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("\nВведите количество трехзначных чисел: ");
+int Quantity = 0;
+Quantity = int.Parse(Console.ReadLine());
+int[] Numbers = new int[Quantity];
+int Index = 0;
+Console.Write("\nДан массив: [");
+for(int i = 0; i < Numbers.Length; i++)
+{
+    Numbers[i] = new Random().Next(100,1000);
+    if(Numbers[i] % 2 == 0)
+    {   
+        Index++;
+    }
+    if(i == Numbers.Length - 1)
+    {
+        Console.WriteLine($"{Numbers[i]}]");    
+        break;
+    }
+    Console.Write($"{Numbers[i]}, ");
+}
+
+Console.WriteLine($"\nКоличество четных чисел в массиве равно {Index}\n");
