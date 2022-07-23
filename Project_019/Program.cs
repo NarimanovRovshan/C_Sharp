b41fcd786e210c375bd101b50d3e25e5754023aa
Narimanov Rovshan notebook <rovshan-az@mail.ru>
2022-07-23 13:09:21 +0300

    Автоматическое заполнеие двумерного массива случайными вещественными числами.

---
+++ Project_019/Program.cs
@@ -0,0 +1,26 @@
+﻿while(true){
+    int Columns;
+    int Lines;
+    Console.Clear();
+    Console.Write("Заполнеие двумерного массива случайными вещественными числами. \n\nВведите размер массива. \nКоличество столбиков: ");
+    Columns = int.Parse(Console.ReadLine());
+    Console.Write("Количество строк: ");
+    Lines = int.Parse(Console.ReadLine());
+    Console.Write("\nРезультат генерации массива:\n");
+
+    double [,] Numbers = new double[Columns,Lines];
+
+    for(int i = 0; i < Numbers.GetLength(0); i++){
+        for(int j = 0; j < Numbers.GetLength(1); j++){
+            Numbers[i,j] = new Random().NextDouble() + new Random().Next(-10,11);
+            Console.Write($"{Math.Round(Numbers[i,j], 1)}\t");
+        }
+        Console.WriteLine("");
+    }
+    
+    Console.Write("\nХотите поробовать ещё раз?(y/n)");
+    char a = char.Parse(Console.ReadLine());
+    if(a != 'y'){
+        break;
+    }
+}
